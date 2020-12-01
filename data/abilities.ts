b71f85f3cd575fb61891b4bdbd527704baecc6be
SmashChamp <owenscotthall@gmail.com>
2020-12-01 05:13:15 -0500

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -308,7 +308,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			if (!lastAttackedBy) return;
 			const damage = move.multihit ? move.totalDamage : lastAttackedBy.damage;
 			if (target.hp <= target.maxhp / 2 && target.hp + damage > target.maxhp / 2) {
-				this.boost({spa: 1});
+				this.boost({spa: 2});
 			}
 		},
 		name: "Berserk",
